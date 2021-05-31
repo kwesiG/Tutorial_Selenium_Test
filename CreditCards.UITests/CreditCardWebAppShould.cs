40e21a1f1757227de85d25391e2598f0d931367c
Kwesi Gepi-Attee <kwesi.gepi.attee@gmail.com>
2021-05-31 20:08:24 +0100

    Add Xunit .dlls, and test class

---
+++ CreditCards.UITests/CreditCardWebAppShould.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+
+namespace CreditCards.UITests
+{
+    public class CreditCardWebAppShould
+    {
+        [Fact]
+        [Trait("Category","Smoke")]
+        public void LoadApplicationPage()
+        {
+            
+        }
+    }
+}
