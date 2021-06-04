1c97bbac9b47c6a8f49ef7646a5730b1d5ea3361
Kwesi Gepi-Attee <kwesi.gepi.attee@gmail.com>
2021-06-04 10:59:26 +0100

    Added reload tests forward and back but not correctly asserted yet..

--- CreditCards.UITests/CreditCardWebAppShould.cs
+++ CreditCards.UITests/CreditCardWebAppShould.cs
@@ -9,13 +9,84 @@ namespace CreditCards.UITests
 {
     public class CreditCardWebAppShould
     {
+        private const string HomeUrl = "http://localhost:44108/";
+        private const string AboutUrl = "http://localhost:44108/Home/About";
+        private const string HomeTitle = "Home Page - Credit Cards";
+
         [Fact]
         [Trait("Category", "Smoke")]
         public void LoadApplicationPage()
         {
+
             using(IWebDriver driver = new FirefoxDriver())
             {
-                driver.Navigate().GoToUrl("http://localhost:44108/");
+                driver.Navigate().GoToUrl(HomeUrl);
+                DemoHelper.Pause();
+
+                Assert.Equal(HomeTitle, driver.Title);
+                Assert.Equal(HomeUrl, driver.Url);
+            }
+        }
+
+        [Fact]
+        [Trait("Category","Smoke")]
+        public void ReloadHomePage()
+        {
+
+            using(IWebDriver driver = new FirefoxDriver())
+            {
+                driver.Navigate().GoToUrl(HomeUrl);
+                DemoHelper.Pause();
+
+                driver.Navigate().Refresh();
+
+                Assert.Equal(HomeTitle, driver.Title);
+                Assert.Equal(HomeUrl, driver.Url);
+            }
+        }
+
+        [Fact]
+        [Trait("Category","Smoke")]
+        public void ReloadHomePageOnBack()
+        {
+
+            using (IWebDriver driver = new FirefoxDriver())
+            {
+                driver.Navigate().GoToUrl(HomeUrl);
+                DemoHelper.Pause();
+                driver.Navigate().GoToUrl(AboutUrl);
+                DemoHelper.Pause();
+                driver.Navigate().Back();
+
+                Assert.Equal(HomeTitle, driver.Title);
+                Assert.Equal(HomeUrl, driver.Url);
+
+                //TODO:  assert that the page was reloaded
+            }
+        }
+
+        [Fact]
+        [Trait("Category","Smoke")]
+        public void ReloadHomePageOnForward()
+        {
+            using(IWebDriver driver = new FirefoxDriver())
+            {
+                driver.Navigate().GoToUrl(AboutUrl);
+                DemoHelper.Pause();
+
+                driver.Navigate().GoToUrl(HomeUrl);
+                DemoHelper.Pause();
+
+                driver.Navigate().Back();
+                DemoHelper.Pause();
+
+                driver.Navigate().Forward();
+                DemoHelper.Pause();
+
+                Assert.Equal(HomeTitle, driver.Title);
+                Assert.Equal(HomeUrl, driver.Url);
+
+                //TODO; Assert that page was reloaded
             }
         }
     }
---
+++ CreditCards.UITests/DemoHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+
+
+namespace CreditCards.UITests
+{
+    internal static class DemoHelper
+    {
+        /// <summary>
+        /// Brief delay to slow down browser interactions for
+        /// demo video recording purposes
+        /// </summary>
+        /// <param name="secondsToPause"></param>
+        public static void Pause(int secondsToPause = 3000)
+        {
+            Thread.Sleep(secondsToPause);
+        }
+        // Worth noting you should not be using thread.sleep in your tests
+    }
+}
